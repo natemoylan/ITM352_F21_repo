c26a5acffdcf1925c3344120c8b194d3e20f0ba7
natemoylan <nmoylan@hawaii.edu>
2021-11-17 00:47:28 -1000

    A1

--- Moylan_Nathaniel_Assignment1/server.js
+++ Moylan_Nathaniel_Assignment1/server.js
@@ -42,10 +42,11 @@ app.get("/display_products", function (request, response) {
     // This function creates a for loop to generate the products for the page
     function display_products() {
         str = '';
+        // loop to generate the products
         for (i = 0; i < products.length; i++) {
             str += `
-            <section class ="items">
-            <h2>${products[i].brand}</h2>
+            <section class ="item">
+            <h2>${products[i].brand}</h2> 
             <h3 label id ="quantity_available${i}"><i>Available: ${products[i].quantity_available} in stock!</i></h3></label>
             <h4>$${products[i].price.toFixed(2)}</h4>
             <img src="./images/${products[i].image}" class="img">
@@ -53,11 +54,100 @@ app.get("/display_products", function (request, response) {
             <input type="text" placeholder="0" name="quantity${i}" onkeyup="checkQuantityTextbox(this);"> 
 
             </section>`;
+
+            // Applies the NonNegInt and checkQuantityTextbox function to make sure the quantity inputted by the user is validated. 
+            if (typeof req.query['purchase_submit'] != 'undefined') {
+        
+                for (i = 0; i < products.length; i++) {
+                    if (params.has(`quantity${i}`)) {
+                        a_qty = params.get(`quantity${i}`);
+                        // make textboxes sticky in case of invalid data
+                        product_selection_form[`quantity${i}`].value = a_qty;
+                        total_qty += a_qty;
+                        if (!isNonNegInt(a_qty)) {
+                            has_errors = true; // oops, invalid quantity
+                            checkQuantityTextbox(product_selection_form[`quantity${i}`]); // show where the error is
+                        }
+                    }
+                }
+                
+                console.log(Date.now() + ': Purchase made from ip ' + req.ip + ' data: ' + JSON.stringify(req.query));
+            }
+            next();
         }
         
         return str;
     }
 });
+
+app.post("/process_invoice", function (request, response, next) {
+    let POST = request.body;
+    if(typeof POST['purchase_submit'] == 'undefined') { // Checks if there is a quantity in the txtbox
+        console.log('No purchase form data'); // Sends message to user
+        next();
+    } 
+
+    console.log(Date.now() + ': Purchase made from ip ' + request.ip + ' data: ' + JSON.stringify(POST));
+
+    var contents = fs.readFileSync('./public/invoice.html', 'utf8');
+    response.send(eval('`' + contents + '`')); // render template string
+
+    function display_invoice_table_rows() {
+        subtotal = 0;
+        str = '';
+        for (i = 0; i < products.length; i++) {
+            a_qty = params.get(`quantity${i}`);
+            if(typeof POST[`quantity${i}`] != 'undefined') {
+                a_qty = POST[`quantity${i}`];
+            }
+            if (a_qty > 0) {
+                // product row
+                extended_price =a_qty * products[i].price
+                subtotal += extended_price;
+                str += (`
+      <tr>
+        <td width="43%">${products[i].brand}</td>
+        <td align="center" width="11%">${a_qty}</td>
+        <td width="13%">\$${products[i].price}</td>
+        <td width="54%">\$${extended_price}</td>
+      </tr>
+      `);
+            }
+        }        
+
+        // Compute tax
+        tax_rate = 0.0575;
+        tax = tax_rate * subtotal;
+
+        // Compute shipping
+        if (subtotal <= 50) {
+            shipping = 2;
+        }
+        else if (subtotal <= 100) {
+            shipping = 5;
+        }
+        else {
+            shipping = 0.05 * subtotal; // 5% of subtotal
+        }
+
+        // Compute grand total
+        total = subtotal + tax + shipping;
+        
+        return str;
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.use(express.static('./public'));
